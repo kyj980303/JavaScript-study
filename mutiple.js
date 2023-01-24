253a38ff9cfec2e07b8117a24439129c23490a28
YJung <rladuswjd0303@naver.com>
2023-01-24 23:56:42 +0900

    delete file

--- mutiple.js
+++
@@ -1,9 +0,0 @@
-/* min부터 max까지 곱하는 메소드 만들기 */
-function multiple(min, max) {
-  let output = 1;
-  for (let i = min; i <= max; i++) {
-    output *= i;
-  }
-  return output;
-}
-console.log(multiple(1, 10));
